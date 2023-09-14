84bde43a4cde3111ab75b27a61c921412388108d
Juliana Maria <juliana.ramos01@fatec.sp.gov.br>
2023-09-14 16:34:01 -0300

    build: initial spring boot project commit

---
+++ src/main/java/com/digitalwave/recrutatech/RecrutatechApplication.java
@@ -0,0 +1,13 @@
+package com.digitalwave.recrutatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalwave/recrutatech/RecrutatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalwave.recrutatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
