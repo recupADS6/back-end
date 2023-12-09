706b081f4c3db0b0785c34895ea4d220f4acc927
Vitor-y <yurivitor022@gmail.com>
2023-12-09 17:17:01 -0300

    login method and new dependencies in the pom

---
+++ src/main/java/com/digitalwave/recrutatech/service/SecurityService.java
@@ -0,0 +1,36 @@
+package com.digitalwave.recrutatech.service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.digitalwave.recrutatech.entity.Autorizacao;
+import com.digitalwave.recrutatech.entity.Usuario;
+import com.digitalwave.recrutatech.repository.UserRepository;
+
+@Service
+public class SecurityService implements UserDetailsService{
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOp = userRepository.findByUsername(username);
+        if(usuarioOp.isEmpty()) {
+            throw new UsernameNotFoundException("Usuário não encontrado!");
+        }
+        Usuario usuario = usuarioOp.get();
+        return User.builder().username(username).password(usuario.getPassword())
+            .authorities(usuario.getAutorizacoes().stream()
+            .map(Autorizacao::getNome).collect(Collectors.toList())
+            .toArray(new String[usuario.getAutorizacoes().size()]))
+        .build();
+    }
+}
